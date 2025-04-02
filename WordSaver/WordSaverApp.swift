b023696de6ff59635f5e4ce47cbc674a4b57504d
d.kheyfets <d.kheyfets@hh.ru>
2025-04-02 22:22:37 +0300

    Initial Commit

---
+++ WordSaver/WordSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordSaverApp.swift
+//  WordSaver
+//
+//  Created by d.kheyfets on 02.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordSaverTests/WordSaverTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordSaverTests.swift
+//  WordSaverTests
+//
+//  Created by d.kheyfets on 02.04.2025.
+//
+
+import Testing
+@testable import WordSaver
+
+struct WordSaverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
